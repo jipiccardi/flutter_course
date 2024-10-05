d8caa4d4a451f2ddd1b80a2bd1cb64b692f75194
jipiccardi <jipiccardi@gmail.com>
2024-10-04 23:00:42 -0300

    Revert "Image picker implementation"
    This reverts commit e9f8ea6b0183bd506a9c83ae6c31d962264daea9.

--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/router/app_router.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 import 'package:widgets_app/config/theme/presentation/providers/theme_provider.dart';
 
 void main() {
