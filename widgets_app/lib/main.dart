e9f8ea6b0183bd506a9c83ae6c31d962264daea9
jipiccardi <jipiccardi@gmail.com>
2024-10-04 22:36:15 -0300

    Image picker implementation

--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/router/app_router.dart';
-import 'package:widgets_app/config/theme/app_theme.dart';
 import 'package:widgets_app/config/theme/presentation/providers/theme_provider.dart';
 
 void main() {
